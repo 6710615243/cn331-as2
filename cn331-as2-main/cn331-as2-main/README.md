b0af5408066cf26f5f48a08a8b784a31eef578ce
Lalita Thatsananunchai <6710615243@student.tu.ac.th>
2025-09-21 13:30:23 +0700

    A02
    งาน A02 หนูส่งตั้งแต่อาทิตย์ที่แล้ว แล้วนะคะ แต่ตอนทำงานใหม่หนูลืมสร้าง deploy แยกไว้เลยกลับกลายเป็นงานเก่ารวมกับงานใหม่ แต่หนูยังมีโค้ดงานเก่าอยู่เลยมา push ใน main อีกรอบค่ะ ส่วนงาน A03 ตอนนี้สร้าง deploy แยกไว้ให้เรียบร้อยแล้วค่ะ

---
+++ cn331-as2-main/cn331-as2-main/README.md
@@ -0,0 +1,32 @@
+# cn331-as2
+// Napattira Loaklemhung 6710615243
+// Lalita Thatsananunchai 6710615243
+
+รายละเอียดของระบบ 
+ระบบพวกหนูจะทำการจองห้องเรียน มีการ login แยกฝั่งรหว่าง user กับ admin
+user 1 คน สามารถจองได้เพียงหนึ่งชม. หากจะจองห้องอื่นแทรต้องทำการ cancle ก่อนถึงจะจองได้ 
+และในการจองแต่ละครั้ง user จะเห็นรายละเอียดของห้องที่ตัวเองจอง และกดยืนยันการจองจะถือเป็นอันเสร็จสิ้น
+
+ส่วนทางฝั่ง admin จะสามารถดูรายละเอียดของห้องเรียน ราบละเอียด user ได้
+โดยภาษาหลักที่พวกหนูใช้จะเป็น python ใช้พัฒนาเว็บในฝั่ง backend ใส่ส่วนของ html เป็นโครงสร้างในส่วนของเว็บและเนื้อหาต่างๆ  และ css จะทำการตกแต่งหน้าเว็บ
+
+admin
+username: admin
+password: 123456
+
+user1:
+username: ln
+password: fdsazxcv2
+
+user2:
+username: mow
+password: fdsazxcv1
+
+user3:
+username: yu
+password: fdsazxcv3
+
+https://github.com/user-attachments/assets/d79eaa47-2f16-4953-a00a-e4791b8e6662
+
+
+
---
+++ cn331-as2-main/cn331-as2-main/classroom/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class RoomData(models.Model):
+    room_code = models.CharField(max_length=6)
+    room_name = models.CharField(max_length=10)
+    room_capacity = models.IntegerField()
+    room_hour = models.IntegerField()
+    room_available = models.BooleanField(default=True)
+
+    #String repretentation of the tour
+    def __str__(self):
+        return (f"ID:{self.id} Room name : {self.room_name} code : {self.room_code} Capacity : {self.room_capacity} hours : {self.room_hour} Available : {self.room_available}" )
+
+
+# Reservation
+class Reservation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(RoomData, on_delete=models.CASCADE)
+    reserved_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} reserved {self.room.room_name}"

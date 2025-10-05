67140d6f4617e44f9360c163b871d2a63fdd29a4
Lalita Thatsananunchai <6710615243@student.tu.ac.th>
2025-10-05 23:12:24 +0700

    A04

---
+++ myproject/setting_test.py
@@ -0,0 +1,9 @@
+import sys
+
+if 'test' in sys.argv:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': ':memory:', 
+        }
+    }

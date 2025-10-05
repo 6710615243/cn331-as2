1ea5f71ff8999e6ccc5830e72a3567480cbb7ffa
Lalita Thatsananunchai <6710615243@student.tu.ac.th>
2025-10-05 23:15:44 +0700

    Delete myproject/setting_test.py

--- myproject/setting_test.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-if 'test' in sys.argv:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': ':memory:', 
-        }
-    }

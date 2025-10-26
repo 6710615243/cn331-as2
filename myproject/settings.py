43a75ed8bd22f6e87f0f2c7b3563272e1ef00d63
Lalita Thatsananunchai <6710615243@student.tu.ac.th>
2025-10-26 15:39:42 +0700

    new database

--- myproject/settings.py
+++ myproject/settings.py
@@ -79,13 +79,13 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-             'NAME': BASE_DIR / 'db.sqlite3',
-        }
+         'default': dj_database_url.parse(
+        "postgresql://roombooking_nqt5_user:zd9CQCcmUcYHb7vhFDegKSD7b4aa074I@dpg-d3urn7odl3ps73fbpm3g-a.abcdef.postgres.database.com:5432/roombooking_nqt5",
+        )
     }
+    
 
-DATABASES['default'] = dj_database_url.parse("postgresql://roombooking_nqt5_user:zd9CQCcmUcYHb7vhFDegKSD7b4aa074I@dpg-d3urn7odl3ps73fbpm3g-a/roombooking_nqt5")
+# DATABASES['default'] = dj_database_url.parse("postgresql://roombooking_nqt5_user:zd9CQCcmUcYHb7vhFDegKSD7b4aa074I@dpg-d3urn7odl3ps73fbpm3g-a/roombooking_nqt5")
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
